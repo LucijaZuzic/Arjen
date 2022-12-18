d52fe1cb2fc950a289c720d4fc3cba3e763db0cc
LucijaZuzic <lzuzic49@gmail.com>
2022-12-18 10:59:15 +0100

    Add number display

--- app/src/main/java/com/example/arjen/activities/Numbers.java
+++ app/src/main/java/com/example/arjen/activities/Numbers.java
@@ -4,11 +4,14 @@ package com.example.arjen.activities;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.speech.tts.TextToSpeech;
+import android.util.DisplayMetrics;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.arjen.R;
 import com.example.arjen.utility.MenuActivity;
 import com.example.arjen.utility.myTTS;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Random;
 
@@ -17,6 +20,7 @@ public class Numbers extends MenuActivity {
     private Integer currentNumber = 0, numberToGuess = 0;
     private MediaPlayer musicDing;
     private boolean answered = false;
+    private LinearLayout numberMarkers, currentNumberMarkers;
 
     @Override
     protected void onStop() {
@@ -36,6 +40,8 @@ public class Numbers extends MenuActivity {
         modeTextView = findViewById(R.id.modeTextView);
         numberTextView = findViewById(R.id.numberTextView);
         numberCurrentTextView = findViewById(R.id.numberCurrentTextView);
+        numberMarkers = findViewById(R.id.numberMarkers);
+        currentNumberMarkers = findViewById(R.id.currentNumberMarkers);
     }
 
     @Override
@@ -49,6 +55,7 @@ public class Numbers extends MenuActivity {
         currentNumber = 0;
         Random randomNew = new Random();
         numberToGuess = randomNew.nextInt(5) + 1;
+        setNumberMarkers();
         Intent intent = getIntent();
         int words;
         if (intent.hasExtra("words")) {
@@ -56,8 +63,8 @@ public class Numbers extends MenuActivity {
         } else {
             words = randomNew.nextInt(2);
         }
-        numberTextView.setText(getResources().getString(R.string.number_to_guess) + ": " + numberToGuess);
-        numberCurrentTextView.setText(getResources().getString(R.string.number_input) + ": 0");
+        numberTextView.setText("" + numberToGuess);
+        numberCurrentTextView.setText("0");
         if (words == 1) {
             modeTextView.setText(getResources().getString(R.string.numbers_words));
             myTTS.speak(numberToGuess.toString(), TextToSpeech.QUEUE_FLUSH);
@@ -81,7 +88,8 @@ public class Numbers extends MenuActivity {
             return;
         }
         currentNumber++;
-        numberCurrentTextView.setText(getResources().getString(R.string.number_input) + ": " + currentNumber);
+        activateNumberMarkers();
+        numberCurrentTextView.setText("" + currentNumber);
         myTTS.speak(currentNumber.toString(), TextToSpeech.QUEUE_FLUSH);
     }
 
@@ -103,6 +111,33 @@ public class Numbers extends MenuActivity {
         }
         musicDing.start();
         musicDing.setOnCompletionListener(v -> fillData());
+    }
+
+    public void setNumberMarkers() {
+        currentNumberMarkers.removeAllViews();
+        numberMarkers.removeAllViews();
+        for (int i = 0; i < numberToGuess; i++) {
+            numberMarkers.addView(createNumberMarker(false));
+        }
+    }
+
+    public void activateNumberMarkers() {
+        currentNumberMarkers.removeAllViews();
+        for (int i = 0; i < Math.min(currentNumber, numberToGuess); i++) {
+            currentNumberMarkers.addView(createNumberMarker(false));
+        }
+        if (numberToGuess < currentNumber) {
+            ((FloatingActionButton) currentNumberMarkers.getChildAt(currentNumberMarkers.getChildCount() - 1)).setImageDrawable(getDrawable(R.drawable.ic_baseline_add_24));
+        }
+    }
+
+    public FloatingActionButton createNumberMarker(boolean enabled) {
+        FloatingActionButton numberMarker = new FloatingActionButton(this);
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        linearLayoutParams.setMargins(10, 25, 10, 25);
+        numberMarker.setLayoutParams(linearLayoutParams);
+        numberMarker.setEnabled(enabled);
+        return numberMarker;
     }
 
     @Override
